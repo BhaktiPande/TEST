969d07e7913792bc84626a3105ea6dbf89b4a1d7
Kumar Prashant <kumar@esopdirect.com>
2021-03-20 15:45:52 +0530

    Revert "Bootstrap 4 Issues Resolved"
    This reverts commit 7cfb87a9eb4eaa093383a0caac9f2b3243a5a870.

--- MyInsider/DBScripts/ScriptsWithDefaultData/000. General Scripts/Update Bootstrap Icons.sql
+++ MyInsider/DBScripts/ScriptsWithDefaultData/000. General Scripts/Update Bootstrap Icons.sql
@@ -3,10 +3,3 @@ BEGIN
 		Update mst_MenuMaster set ImageURL='fa fa-question-circle' where  MenuID in(44,45)
 END
 
--- Change the ColumnAlignment on Intial Disclosures Grid
-IF EXISTS(select * from com_GridHeaderSetting where GridTypeCodeId =  114048 and ResourceKey='dis_grd_17251')
-BEGIN
-		Update com_GridHeaderSetting set ColumnAlignment=155003
-		where GridTypeCodeId =  114048 and ResourceKey='dis_grd_17251'
-END
-
