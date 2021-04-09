a494785c861500e4474c22c4bd67d018c67351b8
sandeshlande <sandesh.ext@esopdirect.com>
2021-04-09 14:52:29 +0530

    Requirement 6494: India RF | Add footnote on PED page of insiders Added Note

---
+++ MyInsider/DBScripts/ScriptsWithDefaultData/000. General Scripts/173. Added footer note in mst_resource.sql
@@ -0,0 +1,8 @@
+--Added mst_resource which used in PeriodStatusOS
+IF NOT EXISTS(SELECT ResourceId FROM mst_Resource 
+                           WHERE ResourceId=64004)
+	BEGIN
+		INSERT INTO mst_Resource VALUES 
+		(64004,'dis_lbl_64004','Please upload your De-mat statements (Transactions and Holdings), if any before clicking on the “Confirm” button.','en-us',103303,104002,122112,
+		'Please upload your De-mat statements (Transactions and Holdings), if any before clicking on the “Confirm” button.',1,dbo.uf_com_GetServerDate())
+	END
