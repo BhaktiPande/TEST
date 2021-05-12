e50c62eb69d4a731ad2fe21eb0f1cf15ffd7f28b
Abdul Rahman <abdul@esopdirect.com>
2021-05-12 16:38:18 +0530

    SSO ESOP Library

---
+++ MyInsider/Application/ESOP.SSO.Library/Generic.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ESOP.SSO.Library
+{
+    public class Generic : IDisposable
+    {
+
+        #region Implementation of Singleton Pattern
+
+        /// <summary>
+        /// This is a singleton Pattern used for DBUtility
+        /// </summary>
+
+        private static Generic _instance;
+
+        /// <summary>
+        /// Constructors of Generic class
+        /// </summary>
+
+        protected Generic()
+        {
+        }
+
+        private DataTable dsAuthnRequest;
+
+        /// <summary>
+        /// Destructors of Generic class
+        /// </summary>
+
+        ~Generic()
+        {
+            Dispose();
+        }
+
+        /// <summary>
+        /// Creating a instance of a DBUtility class
+        /// </summary>
+        /// <returns>returning the instance of an object</returns>
+
+        public static Generic Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Generic();
+            }
+            return _instance;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This method is used to fetch all the company list for whom the Scheduler need to be executed
+        /// </summary>
+        /// <returns>Returns the list of companies in the form of DataTable</returns>
+
+        /// <summary>
+        /// Set The Connection String Value of ESOPManager
+        /// </summary>
+        public string ConnectionStringValue { get; set; }
+        /// <summary>
+        /// Set The SSO LogFile Path
+        /// </summary>
+        public string SsoLogFilePath { get; set; }
+        /// <summary>
+        /// Set The SSO Log Store Location
+        /// </summary>
+        public string SsoLogStoreLocation { get; set; }
+        /// <summary>
+        /// Here Getting the all Company which has ssoconfiguration done.
+        /// </summary>
+        /// <returns>DataTable</returns>
+        internal DataTable GetAllSsoCompanyList()
+        {
+            dsAuthnRequest = new DataTable();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(ConnectionStringValue))
+                {
+                    try
+                    {
+                        using (var cmd = new SqlCommand("PROC_GET_SSOCONFIG_DETAILS", conn))
+                        using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+                            sqlDataAdapter.Fill(dsAuthnRequest);
+                        }
+                    }
+                    catch (Exception ex) { WriteLog.Instance().ExceptionErrorLog(ex); }
+                    finally { conn.Close(); }
+                }
+            }
+            catch (Exception ex) { WriteLog.Instance().ExceptionErrorLog(ex); }
+            return dsAuthnRequest;
+        
+        }
+
+        
+
+        #region Implementing IDisposible
+        /// <summary>
+        /// Method to Dispose the class
+        /// </summary>        
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ MyInsider/Application/ESOP.SSO.Library/SSOProp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ESOP.SSO.Library
+{
+    public class SSO
+    {
+        public int SSOId { get; set; }
+        public int? GroupID { get; set; }
+        public string GroupName { get; set; }
+        public string InsertionType { get; set; }
+        public string CompanyName { get; set; }
+        public string IDP_SP_URL { get; set; }
+        public string DestinationURL { get; set; }
+        public string AssertionConsumerServiceURL { get; set; }
+        public string IssuerURL { get; set; }
+        public string RelayState { get; set; }
+        public string CertificateName { get; set; }
+        public string Certificate { get; set; }
+        public bool Status { get; set; }
+        public string Parameters { get; set; }
+
+        public bool? IsSSOLoginActiveForEmployee { get; set; }
+
+        public bool? IsSSOLoginActiveForCO { get; set; }
+
+        public bool? IsSSOLoginActiveForNonEmployee { get; set; }
+
+        public bool? IsSSOLoginActiveForCorporateUser { get; set; }
+
+        public string X509CertificateFromSAMLResponse { get; set; }
+
+        public string SignatureValueFromSAMLResponse { get; set; }
+
+        public DateTime? NotBeforeSAMLResponse { get; set; }
+        public DateTime? NotOnOrAfterSAMLResponse { get; set; }
+        public string AttributeCompanyFromSAMLResponse { get; set; }
+        public string AttributeEmailFromSAMLResponse { get; set; }
+        public string AttributeEmployeeIDFromSAMLResponse { get; set; }
+        public string NameIDFromSAMLResponse { get; set; }
+        public bool IsValidateSSO { get; set; }
+        public string Message { get; set; }
+        public string UPNFromSAMALResponse { get; set; }
+        
+    }
+}
